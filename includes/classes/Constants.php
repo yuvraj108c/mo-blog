bf0cc7530c8d2ee9920cabee7af0d1e8a18aac05
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2019-10-31 20:05:18 +0400

    updated constants

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -3,13 +3,15 @@ class Constants
 {
     // Path
     public static $usersXmlPath = "data/users.xml";
+    public static $usersXsdPath = "data/xsd/users.xsd";
 
     //Login
     public static $loginFailed = "Invalid username or password.";
 
     // Registration errors
-    public static $passwordsDoNoMatch = "Your passwords don't match";
-    public static $passwordCharacters = "Your password must be between 5 and 30 characters";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
     public static $emailTaken = "This email is already in use";
     public static $usernameTaken = "This username already exists";
+
+    public static $invalidXmlFile = "Invalid XML File.";
 }
