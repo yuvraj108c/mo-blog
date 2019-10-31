30c667a8a00d0f557397514a28a60c7ed4e4ac0b
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2019-10-31 21:06:35 +0400

    created logout handler

---
+++ includes/handlers/logout-handler.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+define("ROOT_URL", "http://localhost/mo-blog/"); 
+
+// Clear all sessions variables
+unset($_SESSION['userLoggedIn']);
+
+// Go to homepage
+header("Location: " . ROOT_URL . "index.php");
