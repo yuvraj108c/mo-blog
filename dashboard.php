95078cb27a332dc04d44795acd64626a1850c525
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2019-11-01 19:28:21 +0400

    show my posts on dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,33 @@
 <?php
     require("includes/classes/Header.php");
     session_start();
+
     // Redirect to homepage if user not logged in
     if(!isset($_SESSION["userLoggedIn"])){
         header("Location: index.php");
     }
 
-    $header = new Header("Dashboard","");
+    $header = new Header("Dashboard","dashboard.css");
     $header->output();
 ?>
 
 <body>
     <?php include "includes/navbar.php"; ?>
-    <h1>Dashboard</h1>
+
+    <section class="posts">
+        <div class="ui container">
+        
+        <?php 
+            require "includes/classes/Constants.php";
+            require "includes/classes/Post.php";
+            
+            $post = new Post();
+            $userPosts = $post->getPostsByUsername($_SESSION["userLoggedIn"]);
+            
+            $post->outputPosts($userPosts);
+        ?>
+
+        </div>
+    </section>
 </body>
 </html>
