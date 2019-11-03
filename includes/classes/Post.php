6ba93af8f5d052bd4fa8bd8cc65f2bcd25ae09f8
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2019-11-03 11:11:24 +0400

    Merged classes function

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -27,5 +27,42 @@ class Post{
             return false;
         }
     }
+    public function createPost($title,$descri,$cat,$imgUrl,$username){
+        $users = simplexml_load_file(Constants::$postsXmlPath);
 
+            // Update xml file
+            $doc = new DOMDocument();
+            $doc->load(Constants::$postsXmlPath);
+            
+            $postElem = $doc->createElement("post");
+            $idAttr = $doc->createAttribute("id");
+            $idAttr->value = sizeof($doc->getElementsByTagName("post")) + 1;
+            
+            $postElem->appendChild($idAttr);
+            $titleElem = $doc->createElement("title", $title);
+            $descriElem = $doc->createElement("description", $descri);
+            $catElem = $doc->createElement("category", $cat);
+            $urlElem = $doc->createElement("imageUrl", $imgUrl);
+            $createByElem = $doc->createElement("author",$username);
+            $createOnElem = $doc->createElement("createdOn",date("Y-m-d"));
+            
+            $postElem->appendChild($titleElem);
+            $postElem->appendChild($descriElem);
+            $postElem->appendChild($catElem);
+            $postElem->appendChild($urlElem);
+            $postElem->appendChild($createByElem);
+            $postElem->appendChild($createOnElem);
+
+            $doc->getElementsByTagName("posts")[0]->appendChild($postElem);
+
+           if (!$doc->schemaValidate(Constants::$postsXsdPath)) {
+                // Invalid xml
+                array_push($this->errorArray, Constants::$invalidXmlFile);
+                return false;
+            }else{
+               // Save file
+                $doc->save(Constants::$postsXmlPath);
+                return true; 
+            }
+         }
 }
