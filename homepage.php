00fa9f650b04b05dbba3acba7c23477a52533d9a
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2019-11-03 10:23:57 +0400

    small changes

--- homepage.php
+++ homepage.php
@@ -2,29 +2,41 @@
 
     require("includes/classes/Header.php");
     session_start();
+
     // Redirect to homepage if user not logged in
     if(!isset($_SESSION["userLoggedIn"])){
         header("Location: index.php");
     }
-
-    $header = new Header("homepage","");
-    $header->output();
-
-    include "includes/navbar.php";
     
-    // Load XML file
-    $xml = new DOMDocument;
-    $xml->load('./data/posts.xml');
+    $header = new Header("homepage","dashboard.css");
+    $header->output();
+?>
 
-    // Load XSL file
-    $xsl = new DOMDocument;
-    $xsl->load('./data/xslt/posts.xsl');
+<body>
+    <?php include "includes/navbar.php"; ?>
 
-    // Configure the transformer
-    $proc = new XSLTProcessor();
+    <section class="posts">
+        <div class="ui container">
 
-    // Attach the xsl rules
-    $proc->importStyleSheet($xsl);
+    <?php
+        
+        // Load XML file
+        $xml = new DOMDocument;
+        $xml->load('./data/posts.xml');
 
-    echo $proc->transformToXML($xml);
-?> 
+        // Load XSL file
+        $xsl = new DOMDocument;
+        $xsl->load('./data/xslt/homepage.xsl');
+
+        // Configure the transformer
+        $proc = new XSLTProcessor();
+
+        // Attach the xsl rules
+        $proc->importStyleSheet($xsl);
+
+        echo $proc->transformToXML($xml);
+    ?> 
+        </div>
+    </section>
+</body>
+</html>
